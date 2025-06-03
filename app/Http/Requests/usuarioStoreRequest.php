9a3a7eb4dbba1dfd0229972de71a266895128cd1
perin <leoandreozi@gmail.com>
2025-06-03 19:53:22 -0300

    created and coded all requests on request folder

---
+++ app/Http/Requests/usuarioStoreRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class usuarioStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "nome"=>"required|string|max:50",
+            "email"=>"required|string|max:100",
+            "senha"=>"required|string|max:24",
+        ];
+    }
+}
